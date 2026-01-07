31ca93a1f69cac2339b7df465b07149f4da93a6d
Carlo-Odoo <jcocampo@avatco.com>
2026-01-07 03:41:43 +0000

    Initial commit

---
+++ library_management/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Library Management',
+    'version': '1.0',
+    'description': 'A module for managing library operations.',
+    'author': 'Carlo',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
