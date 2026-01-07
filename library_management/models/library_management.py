a0a8519f3145a4904bdb1da84dd45f2664c12edd
Carlo-Odoo <jcocampo@avatco.com>
2026-01-07 05:16:43 +0000

    Library management model declaration

---
+++ library_management/models/library_management.py
@@ -0,0 +1,27 @@
+from odoo import models, fields, api, _
+
+class LibraryManagement(models.Model):
+    _name = "library.management"
+
+    title = fields.Char(string="Title")
+    isbn = fields.Integer(
+        string='ISBN',
+        help="Unique13-digit code identifying a specific edition of a book or book-like product"
+    )
+    date_published = fields.Date(
+        string='Date Published',
+        default=fields.Date.context_today,
+    )
+    publisher_id_id = fields.Many2one(
+        string='Publisher',
+        comodel_name='res.partner',
+    )
+    
+    authors_ids = fields.Many2many(
+        string='authors',
+        comodel_name='res.partner',
+    )
+    
+    
+    
+    
