6067aadbcfd361cf030adc43014552e5db50b108
Carlo-Odoo <jcocampo@avatco.com>
2026-01-08 15:10:43 +0000

    Library extension module init

---
+++ library_extension/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Library Management Extension",
+    "version": "1.0",
+    "description": "An extension module for library management user portals.",
+    "category": "Extension",
+    "author": "Carlo",
+    "depends": ["library_management", "contacts", "website"],
+    "images": [
+    ],
+    "data": [
+      
+    ],
+    "installable": True,
+    "application": False,
+}
