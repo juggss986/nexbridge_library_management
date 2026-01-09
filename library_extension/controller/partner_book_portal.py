bfcd2756099285bd7b58bba77b16fe0cb476b08f
Carlo-Odoo <jcocampo@avatco.com>
2026-01-09 03:25:45 +0000

    Pina ganda ng konti ang qweb cards

--- library_extension/controller/partner_book_portal.py
+++ library_extension/controller/partner_book_portal.py
@@ -6,7 +6,8 @@ class BookPortal(http.Controller):
     @http.route(['/my/books'], type='http', auth='user', website=True)
     def portal_books(self, **kw):
         partner_books_ids = request.env.user.partner_id.my_book_ids
+        books = request.env['library.management'].sudo().search([('id', 'in', partner_books_ids.ids)])
         return request.render(
             'library_extension.partner_portal_books_page',
-            {'books': partner_books_ids}
+            {'books': books}
         )
--- library_extension/models/library_management.py
+++ library_extension/models/library_management.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class LibraryManagement(models.Model):
     _inherit = "library.management"
     
-    book_web_like = fields.Char(string="Wikipage Link")
+    book_web_link = fields.Char(string="Wikipage Link")
